63fe08c1da07d50b3f7c38160bb34e4a5a5dea71
TasosTsaousis <tasoskanenas@gmail.com>
2015-08-23 15:24:44 +0300

    working on array methods simulation

--- testCases/simulatingArrayMethods.js
+++ testCases/simulatingArrayMethods.js
@@ -1,1 +1,27 @@
-(window.simulateArrayMethods = function() {})
+(window.simulateArrayMethods = function () {
+
+    var elems = {
+
+        //Stores the count of elements. If we’re going to pretend we’re an array, we’re going to need someplace to store the number of items that we’re storing.
+        length: 0,
+
+        //Implements the method to add elements to our collection. The prototype for Array
+        //already has a method to do this, so why not use it instead of reinventing the wheel?
+        add: function (elem) {
+            Array.prototype.push.call(this, elem);
+        },
+
+        //Implements a method named gather() to find elements by their id values and add them to our collection.
+        gather: function (id) {
+            this.add(document.getElementById(id));
+        }
+    };
+
+    //Tests the gather() and add() methods.
+    elems.gather("first");
+    assert(elems.length == 1 && elems[0].nodeType,
+        "Verify that we have an element in our stash");
+    elems.gather("second");
+    assert(elems.length == 2 && elems[1].nodeType,
+        "Verify the other insertion");
+})
